d10a038b0fe05a881b3943b9cd210f8481bcff44
배주연 <w2316@e-mirim.hs.kr>
2024-03-20 23:24:28 +0900

    업캐스팅과 다운캐스팅

---
+++ JavaStudy2024/src/Extends/Person.java
@@ -0,0 +1,11 @@
+package Extends;
+
+public class Person {
+	String name;
+	String id;
+	
+	public Person(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+}
---
+++ JavaStudy2024/src/Extends/Student.java
@@ -0,0 +1,11 @@
+package Extends;
+
+public class Student extends Person{
+	String grade;
+	String department;
+	
+	public Student(String name, String id) {
+		super(name,id);
+		 
+	}
+}
