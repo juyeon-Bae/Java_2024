1d215907a3cfdec51ea8bb43e1bb3e13cc6000fe
배주연 <w2316@e-mirim.hs.kr>
2024-04-02 22:44:50 +0900

    추상 메소드와 추상 클래스

---
+++ JavaStudy2024/src/Abstract/Calculator.java
@@ -0,0 +1,7 @@
+package Abstract;
+
+abstract public class Calculator {
+	public abstract int add(int a, int b); // 추상 메소드 : 선언되어 있으나 구현되어 있지 않은 메소드
+	public abstract int substract(int a, int b);
+	public abstract double average(int[] a);
+}
